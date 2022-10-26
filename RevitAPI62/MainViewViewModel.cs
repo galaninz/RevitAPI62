1f94f7bf798a9eae6594a43f0bd028da750b2d60
Zakhar <zakhargalanin@gmail.com>
2022-10-26 07:16:58 -0700

    Add project files.

---
+++ RevitAPI62/MainViewViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI62
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public List<Level> Levels { get; } = new List<Level>();
+        public List<FamilySymbol> Furniture { get; } = new List<FamilySymbol>();
+        public DelegateCommand SaveCommand { get; }
+        public List<XYZ> Points { get; } = new List<XYZ>();
+        public FamilySymbol SelectedFurnitureType { get; set; }
+        public Level SelectedLevel { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            Points = SelectionUtils.GetPoints(_commandData, "Выберите точку", ObjectSnapTypes.Points);
+            Levels = LevelsUtils.GetLevels(commandData);
+            Furniture = FamilySymbolUtils.GetFamilySymbols(commandData);
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            
+        }
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            XYZ insertionPoint = Points[0];
+
+            FamilyInstanceUtils.CreateFamilyInstance(_commandData, SelectedFurnitureType, insertionPoint, SelectedLevel);
+
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ RevitAPI62/SelectionUtils.cs
@@ -0,0 +1,82 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI62
+{
+    public class SelectionUtils
+    {
+        public static List<Level> Levels { get; internal set; }
+
+        public static Element PickObject(ExternalCommandData commandData, string message = "Выберите элемент")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObject = uidoc.Selection.PickObject(ObjectType.Element, message);
+            var oElement = doc.GetElement(selectedObject);
+            return oElement;
+        }
+
+        public static T GetObject<T>(ExternalCommandData commandData, string promptMessage)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Reference selectedObj = null;
+            T elem;
+            try
+            {
+                selectedObj = uidoc.Selection.PickObject(ObjectType.Element, promptMessage);
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+            elem = (T)(object)doc.GetElement(selectedObj.ElementId);
+            return elem;
+        }
+
+        public static List<Element> PickObjects(ExternalCommandData commandData, string message = "Выберите элементы")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObjects = uidoc.Selection.PickObjects(ObjectType.Element, message);
+            List<Element> elementList = selectedObjects.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
+            return elementList;
+        }
+
+        public static List<XYZ> GetPoints(ExternalCommandData commandData,
+                            string promptMessage, ObjectSnapTypes objectSnapTypes)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+            List<XYZ> points = new List<XYZ>();
+
+            while (points.Count < 1)
+            {
+                XYZ pickedPoint = null;
+                try
+                {
+                    pickedPoint = uidoc.Selection.PickPoint(objectSnapTypes, promptMessage);
+                }
+                catch (Autodesk.Revit.Exceptions.OperationCanceledException ex)
+                {
+                    break;
+                }
+                points.Add(pickedPoint);
+            }
+
+            return points;
+        }
+    }
+}
